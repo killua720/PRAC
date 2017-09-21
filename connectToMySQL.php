c926357e3cc9e9973df6224bbe67f0212b1a656c
killua720 <4666447@gmail.com>
2017-09-21 14:02:42 +0800

    補充mysql連線帳號密碼,註解物件/程序導向

--- connectToMySQL.php
+++ connectToMySQL.php
@@ -15,12 +15,13 @@
         $password ="secret";
 
         //執行連線到資料庫的動作,並將回傳的東西存放到$dbLink中
-        $dbLink = new mysqli($host, $user, $password);
+        $dbLink = new mysqli($host,$user,$password);
 
         //檢查是否連線錯誤
         if($dbLink->connect_error)
         {
-            die("連線錯誤:".$dbLink->connect_error);
+//            die("連線錯誤:".$dbLink->connect_error);//物件導向
+            die("連線錯誤:".mysqli_connect_error());//程序導向
         }
         else
             echo "連線成功<br>~";
