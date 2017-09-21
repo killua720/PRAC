8ee975d415799dc6be58fe17b7d9a2991682de0b
killua720 <4666447@gmail.com>
2017-09-21 10:35:35 +0800

    刪掉HTML改用PHP

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh_tw">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World!PHP</title>
+</head>
+<body>
+<h1>Finally Success!PHP</h1>
+<h2>哈囉!PHP</h2>
+<?php
+echo "This is after hypertext preprocessor!"
+?>
+</body>
+</html>
